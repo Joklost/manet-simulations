be8818f4a5ed3f4b6ae4d9c19e686497721992cf
Peter G. Jensen <root@petergjoel.dk>
2019-03-27 13:58:13 +0100

    example using colering of edges

--- tools/visualiser/backend/engine.py
+++ tools/visualiser/backend/engine.py
@@ -1,6 +1,7 @@
 import glob
 import math
 
+
 # validate data and proxy to real functions
 def execute(data, files):
     models = list_models()['models']
@@ -48,7 +49,7 @@ def execute(data, files):
                 return file_error(i, data['gps_data'].filename, 'less than four entries')
             id = 0
             lat = 0
-            lon = 0
+            lng = 0
             ts = 0
             try:
                 id = int(entry[0])
@@ -63,10 +64,10 @@ def execute(data, files):
             if lat < -90 or lat > 90:
                 return file_error(i, data['gps_data'].filename, 'entry 0 is not a latitude')
             try:
-                lon = float(entry[2])
+                lng = float(entry[2])
             except Exception:
                 return file_error(i, data['gps_data'].filename, 'entry 0 is not a latitude')
-            if lon < -180 or lon > 180:
+            if lng < -180 or lng > 180:
                 return file_error(i, data['gps_data'].filename, 'entry 0 is not a latitude')
             try:
                 ts = float(entry[3])
@@ -74,7 +75,7 @@ def execute(data, files):
                 return file_error(i, data['gps_data'].filename, 'entry 3 is not a timestamp')
             if ts < 0:
                 return file_error(i, data['gps_data'].filename, 'entry 3 is not a timestamp')
-            parsed.append((id, lat, lon, ts))
+            parsed.append((id, lat, lng, ts))
         fdur = 0
         tdur = -1
         if 'from_duration' in data and len(data['from_duration'].strip()) > 0:
@@ -91,8 +92,10 @@ def execute(data, files):
 
     return {'error': "Unknown type or topology"}
 
+
 def file_error(filename, line, message):
     return {'error': 'Line ' + str(line) + ' of \"' + filename + '\" - ' + message}
+
 
 def list_models():
     res = []
@@ -102,6 +105,7 @@ def list_models():
         model = str(file)
         res.append(model[len(folder): - len(postfix)])
     return {"models": res}
+
 
 def run_static_grid(num_nodes, init_time, duration):
     if num_nodes <= 0:
@@ -116,30 +120,32 @@ def run_static_grid(num_nodes, init_time, duration):
 
     return {}
 
+
 def run_gps(fdur, tdur, data):
     if tdur is not -1 and tdur <= fdur:
         return {'error': 'From duration should be smaller than to duration (' + str(fdur) + ", " + str(tdur) + ")"}
 
     nodes = {}
+    edges = {}
     minlat = math.inf
     maxlat = -math.inf
-    minlon = math.inf
-    maxlon = -math.inf
+    minlng = math.inf
+    maxlng = -math.inf
     mints = math.inf
     maxts = -math.inf
     for row in data:
-        data = {'lat' : row[1], 'lon': row[2], 'timestamp': row[3]}
+        nd = {'lat': row[1], 'lng': row[2], 'timestamp': row[3]}
         if row[3] < fdur or (tdur is not -1 and row[3] > tdur):
             continue
         if row[0] not in nodes:
-            nodes[row[0]] = [data]
+            nodes[row[0]] = [nd]
         else:
-            nodes[row[0]].append(data)
+            nodes[row[0]].append(nd)
         minlat = min(minlat, row[1])
         maxlat = max(maxlat, row[1])
 
-        minlon = min(minlon, row[2])
-        maxlon = max(maxlon, row[2])
+        minlng = min(minlng, row[2])
+        maxlng = max(maxlng, row[2])
 
         mints = min(mints, row[3])
         maxts = max(maxts, row[3])
@@ -147,4 +153,18 @@ def run_gps(fdur, tdur, data):
         return {'error': 'No nodes within the duration (or file is empty or could not be parsed)'}
     for key in nodes:
         nodes[key].sort(key=lambda el: el['timestamp'])
-    return {'nodes': nodes, 'min_lat': minlat, 'max_lat': maxlat, 'min_lon': minlon, 'max_lon': maxlon, 'first_time': mints, 'last_time': maxts}
+    for id in nodes:
+        for step in nodes[id]:
+            for other in nodes:
+                if id == other or nodes[other][0]['timestamp'] > step['timestamp'] or nodes[other][-1]['timestamp'] < step['timestamp']:
+                    continue
+                nd = {'timestamp': step['timestamp'], 'dest': other}
+                if id not in edges:
+                    edges[id] = {}
+                if other not in edges[id]:
+                    edges[id][other] = []
+                edges[id][other].append(nd)
+    for key in edges:
+        for k2 in edges[key]:
+            edges[key][k2].sort(key=lambda el: el['timestamp'])
+    return {'nodes': nodes, 'edges': edges, 'min_lat': minlat, 'max_lat': maxlat, 'min_lng': minlng, 'max_lng': maxlng, 'first_time': mints, 'last_time': maxts}
--- tools/visualiser/static/logic.js
+++ tools/visualiser/static/logic.js
@@ -42,7 +42,7 @@ $(document).ready(function () {
     // initialize the map
     let show_map = function (p) {
         p.setup = function () {
-            map_canvas = p.createCanvas(640, 640);
+            map_canvas = p.createCanvas(p.windowWidth, 640);
             map = mappa.tileMap(options);
             map.overlay(map_canvas)
             map_canvas.fill(200, 100, 100);
@@ -53,6 +53,7 @@ $(document).ready(function () {
         p.drawPoint = function () {
         };
         const diam = 20;
+        const to_diam = 5;
         p.draw = function () {
             if (current_data !== null)
             {
@@ -73,19 +74,39 @@ $(document).ready(function () {
                 }
                 $("#time_val").html(glob_time);
                 $("#speed_val").html("x " + speed);
-                var some = false;
-                var num = 0;
+
+                // draw edges in the bottom
                 for (var id in current_data.nodes)
                 {
-                    var data = node_data(id, glob_time);
+                    const data = node_data(id, glob_time);
+                    if (data === null)
+                        continue;      
+                    const dot = map.latLngToPixel(data);
+                    var edges = node_edges(id, glob_time);
+                    for(var eid in edges)
+                    {      
+                        const odata = node_data(edges[eid].dest, glob_time);
+                        var odot = map.latLngToPixel(odata);
+                        show_edge(p, edges[eid], data, odata, dot, odot);
+                        p.line(dot.x, dot.y, odot.x, odot.y);
+                        // add some top here!
+                    }
+                }
+                
+                var some = false;
+
+                for (var id in current_data.nodes)
+                {
+                    const data = node_data(id, glob_time);
                     if (data === null)
                         continue;
                     some = true;
                     const dot = map.latLngToPixel(data);
-                    p.ellipse(dot.x, dot.y, diam, diam);
+                    show_node(p, data, dot);
+                    p.ellipse(dot.x, dot.y, diam, diam);                    
                     p.text(id, dot.x-(diam/3), dot.y+(diam/4));
-                    ++num;
                 }
+                
                 if (!some)
                 {
                     pause = true;
@@ -123,7 +144,6 @@ $(document).ready(function () {
 
     $("#time_slider").mousedown(function () {
         time_slider_active = true;
-        console.log("Down");
     });
 
     $("#time_slider").change(function () {
@@ -142,31 +162,78 @@ $(document).ready(function () {
     });
 });
 
+function show_node(canvas, data, coordinates)
+{
+    canvas.strokeWeight(1);
+    canvas.stroke(0, 0, 0);
+}
+
+function show_edge(canvas, edge, from, to, from_coord, to_coord)
+{
+    var dist = map.map.distance(from, to);
+    canvas.strokeWeight(3);
+    var v = Math.max(Math.min(255, dist/20));
+    canvas.stroke(v, v, v);
+}
+
+function extrapolate_node(pre, post, delta)
+{
+    return pre;
+}
+
+function extrapolate_edge(pre, post, delta)
+{
+    return pre;
+}
+
 function node_data(id, time)
 {
-    if (parseFloat(current_data.nodes[id][0].timestamp) > glob_time ||
-            parseFloat(current_data.nodes[id][current_data.nodes[id].length - 1].timestamp) < glob_time)
+    if (current_data.nodes[id][0].timestamp > time ||
+            current_data.nodes[id][current_data.nodes[id].length - 1].timestamp < time)
         return null;
     var post = 0;
-    while (parseFloat(current_data.nodes[id][post].timestamp) < glob_time)
-    {
-        post = post + 1;
-    }
+    while (current_data.nodes[id][post].timestamp < time)
+        ++post;
 
-    var lat;
-    var lon;
     if (post === 0)
     {
-        lat = current_data.nodes[id][post].lat;
-        lon = current_data.nodes[id][post].lon;
-    } else
+        return current_data.nodes[id][post];
+    } 
+    else
     {
-        var part = parseFloat(current_data.nodes[id][post].timestamp) - parseFloat(current_data.nodes[id][post - 1].timestamp);
-        part = (glob_time - parseFloat(current_data.nodes[id][post - 1].timestamp)) / part;
-        lat = (current_data.nodes[id][post - 1].lat) + (current_data.nodes[id][post].lat - current_data.nodes[id][post - 1].lat)*part;
-        lon = (current_data.nodes[id][post - 1].lon) + (current_data.nodes[id][post].lon - current_data.nodes[id][post - 1].lon)*part;
+        var part = current_data.nodes[id][post].timestamp - current_data.nodes[id][post - 1].timestamp;
+        part = (time - current_data.nodes[id][post - 1].timestamp) / part;
+        var pre = $.extend(true, {}, current_data.nodes[id][post - 1]);
+        var postd = current_data.nodes[id][post];
+        pre.lat += (postd.lat - pre.lat)*part;
+        pre.lng += (postd.lng - pre.lng)*part;    
+        return extrapolate_node(pre, postd, part);
     }
-    return {'lat': lat, 'lng': lon}
+}
+
+function node_edges(id, time)
+{
+    var res = [];
+    if (current_data.edges[id] == null) return res;
+        
+    for(var eid in current_data.edges[id])
+    {
+        if (current_data.edges[id][eid][0].timestamp > time ||
+                current_data.edges[id][eid][current_data.edges[id][eid].length - 1].timestamp < time)
+            continue;
+        var post = 0;
+        while(current_data.edges[id][eid][post].timestamp < time)
+            ++post;
+        if (post === 0)
+            res.push(current_data.edges[id][eid][post]);
+        else
+        {
+            var part = current_data.edges[id][eid][post].timestamp - current_data.edges[id][eid][post-1].timestamp; 
+                part = (time - current_data.edges[id][eid][post - 1].timestamp) / part;
+                res.push(extrapolate_edge($.extend(true, {}, current_data.edges[id][eid][post - 1]), current_data.nodes[id][post], part));
+        }
+    }
+    return res;
 }
 
 function update_settings(el)
@@ -251,16 +318,8 @@ function add_models(models)
 // lets do the actual display!
 function show_simulation(data)
 {
-    /*    var canvas = $("#visualization");
-     canvas.html("");
-     const mappa_options = {
-     lat: (data.min_lat + data.max_lat)/2,
-     lng: (data.min_lon + data.max_lon)/2,
-     zoom: 4,
-     style: "http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-     }
-     
-     */
+    if(data === {})
+        return;
     $("#speed_slider").attr('min', -100);
     $("#speed_slider").attr('max', 99);
     $("#speed_slider").val(0);
@@ -270,6 +329,6 @@ function show_simulation(data)
     glob_time = data.first_time;
     current_data = data;
 
-    var bounds = [[data.min_lat, data.min_lon], [data.max_lat, data.max_lon]];
+    var bounds = [[data.min_lat, data.min_lng], [data.max_lat, data.max_lng]];
     map.map.fitBounds(bounds);
 }
