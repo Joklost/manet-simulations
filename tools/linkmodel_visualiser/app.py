4eedabde8abc3e7482c10d115f47fc434bde82d9
r5hej <charliebyrdam@gmail.com>
2019-04-22 10:14:36 +0200

    Refactored linkmodel visualiser

--- tools/linkmodel_visualiser/app.py
+++ tools/linkmodel_visualiser/app.py
@@ -1,4 +1,4 @@
-from typing import Tuple, List, Dict
+from typing import Tuple, List, Dict, Any, NoReturn
 
 import os
 import json
@@ -9,6 +9,7 @@ import plotly.offline
 import plotly.graph_objs as go
 import plotly.figure_factory as ff
 from models import LinkPair, Link
+import datetime
 
 
 def parse(file: str) -> Dict[float, List[LinkPair]]:
@@ -86,14 +87,13 @@ def x_axis_to_string() -> List[str]:
     return [f'{x}]' for x in gen_x_axis()]
 
 
-def compute_std_dev_and_mean(data: List[LinkPair]) -> Tuple[float, float]:
+def compute_std_dev_and_mean(data: List[float]) -> Tuple[float, float]:
     """
     Compute the standard deviation and mean for sample data
 
     :param data: The LinkPairs
     :return: [standard deviant, mean]
     """
-    data = [i.rssi for i in data]
     mean = sum(data) / len(data)
 
     x = 0
@@ -103,123 +103,162 @@ def compute_std_dev_and_mean(data: List[LinkPair]) -> Tuple[float, float]:
     return math.sqrt(x / (len(data) - 1)), mean
 
 
+def format_trace_data(data: Dict[float, List[LinkPair]]) -> Dict[int, List[float]]:
+    angle_buckets = gen_x_axis()
+
+    # parse data to dict {angle: sample within angle range}
+    sorted_data = {key: [] for key in angle_buckets}
+    for time in data.keys():
+        for i, angle in enumerate(angle_buckets):
+            # if i == 0:
+            #     bucket_data = [j.rssi for j in data[time] if j.angle <= angle]
+            # else:
+            #     bucket_data = [j.rssi for j in data[time] if angle_buckets[i - 1] < j.angle <= angle]
+            # print(len(bucket_data))
+
+            bucket_data = [j.rssi for j in data[time] if j.angle <= angle] if i == 0 else [j.rssi for j in data[time] if
+                                                                                           angle_buckets[
+                                                                                               i - 1] < j.angle <= angle]
+            sorted_data[angle] = sorted_data[angle] + bucket_data
+
+    return sorted_data
+
+
+def build_normal_plot(data: Dict[int, List[float]]) -> plotly.offline.plot:
+    traces = []
+    group_label = []
+
+    for angle, bucket in data.items():
+        if len(bucket) > 1:
+            std_dev, mean = compute_std_dev_and_mean(bucket)
+            normal_dist = np.random.normal(mean, std_dev, 10000)
+            traces.append(normal_dist)
+            group_label.append(f'{angle}]')
+
+    fig = ff.create_distplot(traces, group_label, curve_type='normal', show_rug=False,
+                             show_hist=False)
+    fig['layout'].update(title='Normal Distributions', xaxis=dict(title='RSSI'),
+                         yaxis=dict(title='Probability density'))
+    return plotly.offline.plot(fig, include_plotlyjs=False, output_type='div')
+
+
+def build_sample_size_trace(data: Dict[int, List[float]]) -> go.Bar:
+    group = []
+    samples = []
+    for angle, bucket in data.items():
+        samples.append(len(bucket))
+        group.append(angle)
+
+    return go.Bar(
+        x=group,
+        y=samples,
+        width=0.3,
+        yaxis='y2',
+        name='Sample size'
+    )
+
+
+def build_rssi_trace(data: Dict[int, List[float]], name: str) -> go.Scatter:
+    group = []
+    rssi = []
+
+    for angle in gen_x_axis():
+        bucket = data[angle]
+        if len(bucket) < 1:
+            continue
+
+        avg = 0
+        for j in bucket:
+            avg += j
+        rssi.append(avg / len(bucket))
+        group.append(angle)
+
+    return go.Scatter(
+        x=group,
+        y=rssi,
+        name=name,
+        mode='lines+markers'
+    )
+
+
+def build_traces(data: Dict[float, List[LinkPair]], log: str, org_trace: go.Scatter) -> NoReturn:
+    # build trace for original log
+    sorted_data = format_trace_data(data)
+
+    # break of all bucket are empty. This happens when then the network is initialized
+    is_empty = True
+    for angle, bucket in sorted_data.items():
+        if len(bucket) > 1:
+            is_empty = False
+
+    if is_empty:
+        return
+
+    # build sample size trace
+    sample_size_trace = build_sample_size_trace(sorted_data)
+
+    # build heuristic trace
+    heuristic_trace = build_rssi_trace(sorted_data, log)
+
+    # build normal distribution plot
+    normal_plot = build_normal_plot(sorted_data)
+
+    layout = go.Layout(
+        xaxis=go.layout.XAxis(
+            tickmode='array',
+            tickvals=gen_x_axis(),
+            ticktext=x_axis_to_string(),
+            showline=True,
+            title='Angle buckets'
+        ),
+        yaxis=go.layout.YAxis(
+            showline=True,
+            showgrid=False,
+            dtick=5,
+            title='RSSI'
+        ),
+        yaxis2=dict(
+            side='right',
+            title='Sample size',
+            overlaying='y',
+            showline=True,
+            tickmode='auto',
+            showgrid=False
+        ),
+        showlegend=True,
+        title=f'Heuristic comparison'
+    )
+
+    fig = go.Figure(data=[org_trace, heuristic_trace, sample_size_trace], layout=layout)
+    rssi_div = plotly.offline.plot(fig, include_plotlyjs=True, output_type='div')
+
+    with open(f'plots/Original-{log}.html', 'w') as f:
+        f.writelines(rssi_div)
+        f.writelines(normal_plot)
+
+
 if __name__ == '__main__':
     log_path = '../../src/logs/parsed'
     logs = os.listdir(log_path)
     link_pairs = {}
 
     for log in logs:
-        # f_name, f_ext = os.path.splitext(log)
-        link_pairs[log] = parse(f'{log_path}/{log}')
+        if log.endswith('.csv'):
+            f_name, _ = os.path.splitext(log)
+        else:
+            f_name = log
 
-    link_pairs_org = link_pairs['original']
+        link_pairs[f_name] = parse(f'{log_path}/{f_name}')
 
+    org_trace = build_rssi_trace(format_trace_data(link_pairs['original']), 'original')
     for log in [i for i in logs if not i == 'original']:
-        time = 497240000.0
+        if log.endswith('.csv'):
+            f_name, _ = os.path.splitext(log)
+        else:
+            f_name = log
 
-        rssi_traces = []
-        org_rssi_data = []
-        rssi_data = []
-        rssi_group = []
-
-        normal_traces = []
-        normal_group_label = []
-
-        sample_size_traces = []
-
-        angle_buckets = gen_x_axis()
-        for i, bucket in enumerate(angle_buckets):
-            if i == 0:
-                org_bucket_data = [j for j in link_pairs_org[time] if j.angle <= bucket]
-                bucket_data = [j for j in link_pairs[log][time] if j.angle <= bucket]
-            else:
-                org_bucket_data = [j for j in link_pairs_org[time] if angle_buckets[i - 1] < j.angle <= bucket]
-                bucket_data = [j for j in link_pairs[log][time] if angle_buckets[i - 1] < j.angle <= bucket]
-
-            if len(org_bucket_data) < 1 or len(bucket_data) < 1:
-                continue
-
-            # compute the sample size for each bucket
-            sample_size_traces.append(len(org_bucket_data))
-
-            # compute the rssi traces
-            org_avg = 0
-            for j in org_bucket_data:
-                org_avg += j.rssi
-            org_rssi_data.append(org_avg / len(org_bucket_data))
-
-            avg = 0
-            for j in bucket_data:
-                avg += j.rssi
-            rssi_data.append(avg / len(bucket_data))
-            rssi_group.append(bucket)
-
-            # compute the normal distribution for each bucket
-            if len(org_bucket_data) > 1:
-                std_dev, mean = compute_std_dev_and_mean(org_bucket_data)
-                normal_dist = np.random.normal(mean, std_dev, 10000)
-                normal_traces.append(normal_dist)
-                normal_group_label.append(f'{bucket}]')
-
-        # add the rssi traces
-        rssi_traces.append(go.Scatter(
-            x=rssi_group,
-            y=org_rssi_data,
-            name='Original',
-            mode='lines+markers'
-        ))
-        rssi_traces.append(go.Scatter(
-            x=rssi_group,
-            y=rssi_data,
-            name=log,
-            mode='lines+markers'
-        ))
-
-        # add the sample size trace
-        rssi_traces.append(go.Bar(
-            x=rssi_group,
-            y=sample_size_traces,
-            width=0.3,
-            yaxis='y2',
-            name='Sample size'
-        ))
-        layout = go.Layout(
-            xaxis=go.layout.XAxis(
-                tickmode='array',
-                tickvals=gen_x_axis(),
-                ticktext=x_axis_to_string(),
-                showline=True,
-                title='Angle buckets'
-            ),
-            yaxis=go.layout.YAxis(
-                showline=True,
-                showgrid=False,
-                dtick=5,
-                title='RSSI'
-            ),
-            yaxis2=dict(
-                side='right',
-                title='Sample size',
-                overlaying='y',
-                showline=True,
-                tickmode='auto',
-                showgrid=False
-            ),
-            showlegend=True,
-            title=f'Heuristic comparison'
-        )
-        fig = go.Figure(data=rssi_traces, layout=layout)
-        rssi_div = plotly.offline.plot(fig, include_plotlyjs=True, output_type='div')
-
-        # add the normal distribution graph
-        normal_fig = ff.create_distplot(normal_traces, normal_group_label, curve_type='normal', show_rug=False,
-                                        show_hist=False)
-        normal_fig['layout'].update(title='Normal Distributions', xaxis=dict(title='RSSI'),
-                                    yaxis=dict(title='Probability density'))
-        normal_div = plotly.offline.plot(normal_fig, include_plotlyjs=False, output_type='div')
-
-        with open(f'plots/Original-{log}.html', 'w') as f:
-            f.writelines(rssi_div)
-            f.writelines(normal_div)
+        print(datetime.datetime.now())
+        build_traces(link_pairs[f_name], f_name, org_trace)
+        print(datetime.datetime.now())
 
     print("done")
