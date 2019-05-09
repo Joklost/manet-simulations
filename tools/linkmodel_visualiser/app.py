7ec1346065204297e9348e04d4e86af4b0b04646
r5hej <charliebyrdam@gmail.com>
2019-05-09 13:09:54 +0200

    Updated the plot generating script

--- tools/linkmodel_visualiser/app.py
+++ tools/linkmodel_visualiser/app.py
@@ -1,4 +1,4 @@
-from typing import Tuple, List, Dict, NoReturn
+from typing import Tuple, List, Dict, NoReturn, Union
 
 import os
 import pprint
@@ -8,12 +8,19 @@ import plotly.plotly as py
 import plotly.offline
 import plotly.graph_objs as go
 import plotly.figure_factory as ff
+import sys
+import pandas as pd
+
 from models import LinkPair, Link
+from angle_rssi import parse_angle_bucket_format, make_dirt_plot
+import helpers
 
 
-def parse(file: str) -> Dict[float, List[LinkPair]]:
+def create_linkpairs(file: str, as_pair: bool = True, lower_bound=-1000, upper_bound=1000,
+                     remove_distance: bool = False) -> Union[Dict[float, List[LinkPair]], List[Link]]:
     data = {}
     link_pairs: Dict[float, List[LinkPair]] = {}
+    all_links: List[Link] = []
 
     with open(file, 'r') as f:
         for line in f:
@@ -22,7 +29,7 @@ def parse(file: str) -> Dict[float, List[LinkPair]]:
             if len(split) <= 4:
                 continue
             # id; lat; lon; timestamp; n0_id; n0_rssi...
-            if f'{split[0]}' not in data.keys():
+            if str(split[0]) not in data.keys():
                 data[f'{split[0]}'] = {}
 
             d = {
@@ -33,11 +40,11 @@ def parse(file: str) -> Dict[float, List[LinkPair]]:
 
             rssi_fields = split[4:]
             for node in range(0, len(rssi_fields), 2):
-                d['nodes'][f'{rssi_fields[node]}'] = rssi_fields[node + 1]
+                d['nodes'][str(rssi_fields[node])] = rssi_fields[node + 1]
 
-            data[f'{split[0]}'][f'{split[3]}'] = d
+            data[str(split[0])][str(split[3])] = d
 
-    for _, e_root_node in data.items():
+    for id_root_node, e_root_node in data.items():
         for timestamp, e_time in e_root_node.items():
             links: List[Link] = []
             node_count = len(e_time['nodes'])
@@ -49,260 +56,273 @@ def parse(file: str) -> Dict[float, List[LinkPair]]:
 
             node_tuples = [[key, val] for key, val in e_time['nodes'].items()]
             for i, pair in enumerate(node_tuples):
-                if timestamp not in data[pair[0]].keys():
+                if pair[0] not in data.keys() or timestamp not in data[pair[0]].keys():
                     continue
 
                 lat = data[pair[0]][timestamp]['lat']
                 lon = data[pair[0]][timestamp]['lon']
-                links.append(Link(root_lat, root_lon, lat, lon, pair[1]))
 
-                for pair2 in node_tuples[i + 1:]:
-                    if pair2[0] not in data[pair2[0]].keys():
-                        continue
+                if not float(pair[1]) == -128.0 and lower_bound <= float(pair[1]) <= upper_bound:
+                    links.append(Link(root_lat, root_lon, lat, lon, pair[1], timestamp, id_root_node, pair[0],
+                                      remove_distance=remove_distance))
 
-                    links.append(Link(lat, lon, data[pair2[0]][timestamp]['lat'], data[pair2[0]][timestamp]['lon'],
-                                      data[pair2[0]][timestamp]['nodes'][pair[0]]))
+                # for pair2 in node_tuples[i + 1:]:
+                #     if pair2[0] not in data.keys() or pair2[0] not in data[pair2[0]].keys():
+                #         continue
+                #
+                #     links.append(Link(lat, lon, data[pair2[0]][timestamp]['lat'], data[pair2[0]][timestamp]['lon'],
+                #                       data[pair2[0]][timestamp]['nodes'][pair[0]]))
 
-            link_pairs[float(timestamp)] = []
-            for i, l1 in enumerate(links):
-                for l2 in links[i + 1:]:
-                    link_pairs[float(timestamp)].append(LinkPair(l1, l2))
+            if as_pair:
+                link_pairs[float(timestamp)] = []
+                for i, l1 in enumerate(links):
+                    for l2 in links[i + 1:]:
+                        if l1.has_common_node(l2):
+                            link_pairs[float(timestamp)].append(LinkPair(l1, l2))
+            else:
+                all_links += links
 
-    return link_pairs
-
-
-def gen_x_axis() -> List[int]:
-    x = []
-    i = 0
-    while i <= 180:
-        x.append(i)
-        i += round(math.sqrt(20))
-
-    return x
+    return link_pairs if as_pair else all_links
 
 
-def x_axis_to_string() -> List[str]:
-    return [f'{x}]' for x in gen_x_axis()]
+def _gen_angle_buckets(interval: int = 5) -> List[int]:
+    return [i for i in range(interval, 181, interval)]
 
 
-def compute_std_dev_and_mean(data: List[float]) -> Tuple[float, float]:
-    """
-    Compute the standard deviation and mean for sample data
-
-    :param data: The LinkPairs
-    :return: [standard deviantion, mean]
-    """
-    mean = sum(data) / len(data)
-
-    x = 0
-    for i in data:
-        x += pow(mean - i, 2)
-
-    return math.sqrt(x / (len(data) - 1)), mean
+def _str_angle_bucket(interval: int = 5) -> List[str]:
+    return [f'0-{interval}'] + [f'{i - interval + 1}-{i}' for i in range(interval * 2, 181, interval)]
 
 
-def format_trace_data(data: Dict[float, List[LinkPair]]) -> Dict[int, List[float]]:
-    angle_buckets = gen_x_axis()
+def parse_to_angle_bucket_individual_rssi_distance(data: Dict[float, List[LinkPair]], as_average: bool = False) \
+        -> Dict[int, List[Tuple[float, float]]]:
+    angle_buckets = _gen_angle_buckets()
 
-    # parse data to dict {angle: sample within angle range}
     sorted_data = {key: [] for key in angle_buckets}
     for time in data.keys():
         for i, angle in enumerate(angle_buckets):
-            bucket_data = [j.rssi for j in data[time] if j.angle <= angle] if i == 0 else \
-                [j.rssi for j in data[time] if angle_buckets[i - 1] < j.angle <= angle]
+            bucket_data = []
+            for j in data[time]:
+                if j.angle <= angle if i is 0 else angle_buckets[i - 1] < j.angle <= angle:
+                    if as_average:
+                        bucket_data.append(j)
+                    else:
+                        bucket_data.append([j.link1.rssi, j.link1.distance])
+                        bucket_data.append([j.link2.rssi, j.link2.distance])
+
             sorted_data[angle] = sorted_data[angle] + bucket_data
 
     return sorted_data
 
 
-def build_normal_plot(data: Dict[int, List[float]]) -> plotly.offline.plot:
-    traces = []
-    group_label = []
-
-    for angle, bucket in data.items():
-        if len(bucket) > 1:
-            std_dev, mean = compute_std_dev_and_mean(bucket)
-            normal_dist = np.random.normal(mean, std_dev, 10000)
-            traces.append(normal_dist)
-            group_label.append(f'{angle}]')
-
-    fig = ff.create_distplot(traces, group_label, curve_type='normal', show_rug=False,
-                             show_hist=False)
-    fig['layout'].update(title='Normal Distributions', xaxis=dict(title='RSSI'),
-                         yaxis=dict(title='Probability density'))
-    return plotly.offline.plot(fig, include_plotlyjs=False, output_type='div')
+def parse_logfile_name(log: str) -> str:
+    return os.path.split(log)[1].split('.')[0]
 
 
-def build_sample_size_trace(data: Dict[int, List[float]]) -> go.Bar:
-    """
-    Build an sample size trace
+def parse_linkpairs_to_angle_bucket_sorted(data, interval=None) -> Tuple[int, Dict[int, List[float]]]:
+    angle_buckets = _gen_angle_buckets() if interval is None else _gen_angle_buckets(interval=interval)
+    fixed = {key: [] for key in angle_buckets}
+    count = 0
 
-    :param data: The data
-    :return: plotly Bar trace
-    """
-    group = []
-    samples = []
-    for angle, bucket in data.items():
-        samples.append(len(bucket))
-        group.append(angle)
+    for time in data.keys():
+        for i, angle in enumerate(angle_buckets):
+            bucket_data = []
+            for lp in data[time]:
+                if lp.angle <= angle if i == 0 else angle_buckets[i - 1] < lp.angle <= angle:
+                    bucket_data.append(lp.rssi_diff)
 
-    return go.Bar(
-        x=group,
-        y=samples,
-        width=0.3,
-        yaxis='y2',
-        name='Sample size'
+            fixed[angle] += bucket_data
+            count += len(bucket_data)
+
+    return count, fixed
+
+
+def build_average_rssi_for_distance_plot(log: str):
+    measured_log = create_linkpairs(log, as_pair=False)
+    bar_y = []
+    scatter_y = []
+    x = []
+
+    count = 0
+    prev = 0
+    max_dist = max([val.distance for val in measured_log])
+    for dist in range(0, int(max_dist), 20):
+        bucket = [link.rssi for link in measured_log if prev < link.distance <= dist]
+
+        if len(bucket) < 1:
+            prev = dist
+            continue
+
+        count += len(bucket)
+        avg_rssi = sum(bucket) / len(bucket)
+
+        bar_y.append(avg_rssi)
+        x.append(f'{prev + 1}-{dist}')
+        prev = dist
+
+    data = [go.Bar(
+        x=x,
+        y=bar_y,
+        name='Average RSSI',
+        width=0.2
+    ), go.Scatter(
+        x=x,
+        y=[26 - Link.l_d(dist) for dist in range(1, int(max_dist), 20)],
+        name='Distance function'
+    )]
+
+    log_name = parse_logfile_name(log)
+    layout = go.Layout(
+        title=f'{log_name} data - The average RSSI in each distance bucket raw - Sample size: {count}',
+        xaxis=dict(
+            title='Distance bucket'
+        ),
+        yaxis=dict(
+            title='Average RSSI'
+        )
     )
+    fig = go.Figure(data=data, layout=layout)
+    plotly.offline.plot(fig, filename=f'plots/The_average_RSSI_in_each_distance_bucket_sep{log_name}.html')
 
 
-def build_rssi_trace(data: Dict[int, List[float]], name: str) -> go.Scatter:
-    """
-    Build an RSSI trace
+def build_angle_bucket_stochastic_minus_distance_fading_increase_plot(log: str):
+    measured_log = create_linkpairs(log, remove_distance=True)
+    count, fixed = parse_linkpairs_to_angle_bucket_sorted(measured_log)
 
-    :param data: The data
-    :param name: Name of the trace to appear in legend
-    :return: plotly Scatter trace
-    """
-    group = []
-    rssi = []
+    x = []
+    y = []
+    for i, angle in enumerate(_gen_angle_buckets()):
+        if len(fixed[angle]) < 1:
+            continue
 
-    for angle in gen_x_axis():
-        bucket = data[angle]
+        x.append(_str_angle_bucket()[i])
+        y.append(sum(fixed[angle]) / len(fixed[angle]))
+
+    data = [go.Bar(
+        x=x,
+        y=y
+    )]
+
+    log_name = parse_logfile_name(log)
+    layout = go.Layout(
+        title=f'{log_name} data - average RSSI minus distance fading for angle buckets - sample size: {count}',
+        xaxis=dict(
+            title='Angle bucket'
+        ),
+        yaxis=dict(
+            title='Average RSSI minus distance'
+        )
+    )
+    fig = go.Figure(data=data, layout=layout)
+    plotly.offline.plot(fig, filename=f'plots/average_RSSI_minus_distance_fading_for_angle_buckets_{log_name}.html')
+
+
+def build_normal_dist_for_angle_bucket(log: str):
+    measured_log = create_linkpairs(log, remove_distance=True)
+    count, fixed = parse_linkpairs_to_angle_bucket_sorted(measured_log)
+    data = []
+    group_labels = []
+
+    for i, angle in enumerate(_gen_angle_buckets()):
+        if len(fixed[angle]) < 1:
+            continue
+
+        std_dev, mean = helpers.compute_std_dev_and_mean(fixed[angle])
+        data.append(np.random.normal(mean, std_dev, 10000))
+
+        group_labels.append(_str_angle_bucket()[i])
+
+    log_name = parse_logfile_name(log)
+    fig = ff.create_distplot(data, group_labels, curve_type='normal', show_rug=False,
+                             show_hist=False, show_curve=True)
+    fig['layout'].update(
+        title=f'{log_name} data - Normal distributions for raw RSSI measurements for angle buckets - Sample size: {count}',
+        xaxis=dict(title='RSSI'),
+        yaxis=dict(title='Probability density'))
+    # data = [go.Histogram(
+    #
+    # )]
+
+    plotly.offline.plot(fig,
+                        filename=f'plots/normal_distributions_for_raw_RSSI_measurements_for_angle_buckets_{log_name}.html')
+
+
+def build_normal_dist_for_distance_bucket(log: str):
+    measured_log = create_linkpairs(log, as_pair=False, remove_distance=True)
+
+    data = []
+    group_labels = []
+    count = 0
+    prev = 0
+    for distance in range(0, int(max([val.distance for val in measured_log])), 50):
+        bucket = [link.rssi for link in measured_log if prev < link.distance <= distance]
         if len(bucket) < 1:
             continue
+            prev = distance
 
-        avg = 0
-        for j in bucket:
-            avg += j
-        rssi.append(avg / len(bucket))
-        group.append(angle)
+        std_dev, mean = helpers.compute_std_dev_and_mean(bucket)
+        data.append(np.random.normal(mean, std_dev, 10000))
+        group_labels.append(f'{prev + 1}-{distance}')
+        prev = distance
+        count += len(bucket)
 
-    return go.Scatter(
-        x=group,
-        y=rssi,
-        name=name,
-        mode='lines+markers'
-    )
+    log_name = parse_logfile_name(log)
+    fig = ff.create_distplot(data, group_labels, curve_type='normal', show_rug=False,
+                             show_hist=False, show_curve=True)
+    fig['layout'].update(
+        title=f'{log_name} data - Normal distributions for raw RSSI measurements for distance buckets - Sample size: {count}',
+        xaxis=dict(title='RSSI'),
+        yaxis=dict(title='Probability density'))
+
+    plotly.offline.plot(fig,
+                        filename=f'plots/normal_distributions_for_raw_RSSI_measurements_for_distance_buckets_{log_name}.html')
 
 
-def compute_score(data: Dict[int, List[float]], org_data: Dict[int, List[float]]) -> float:
-    """
-    Compute the score based on the difference of each RSSI value
+def build_histogram_for_stochastic_in_angle_buckets(log: str):
+    measured_log = create_linkpairs(log, remove_distance=True)
+    count, fixed = parse_linkpairs_to_angle_bucket_sorted(measured_log)
 
-    :return: Score
-    """
-    score = 0.0
-    for angle, bucket in data.items():
-        if len(bucket) < 1 or len(org_data[angle]) < 1:
+    data = []
+    for i, angle in enumerate(_gen_angle_buckets()):
+        if len(fixed[angle]) < 1:
             continue
 
-        avg = 0.0
-        for val in bucket:
-            avg += val
-        avg = avg / len(bucket)
+        data.append(go.Histogram(
+            x=[val / sum(fixed[angle]) for val in fixed[angle]],
+            name=_str_angle_bucket()[i],
+            # histnorm='percent'
+        ))
 
-        org_avg = 0.0
-        for val in org_data[angle]:
-            org_avg += val
-        org_avg = org_avg / len(org_data[angle])
-
-        score += math.pow(abs(org_avg - avg), 2)
-
-    return score
-
-
-def build_traces(data: Dict[float, List[LinkPair]], org_data: Dict[int, List[float]], log: str) -> NoReturn:
-    print('Parsing data')
-    sorted_data = format_trace_data(data)
-
-    # TODO: write to debug log
-    # with open('debug_log_computed', 'w') as f:
-    #     for val in sorted_data[4]:
-    #         f.write(f'{val}\n')
-
-    # break if all buckets are empty. This happens when the network is initialized
-    is_empty = True
-    for angle, bucket in sorted_data.items():
-        if len(bucket) > 1:
-            is_empty = False
-
-    if is_empty:
-        return
-
-    # build sample size trace
-    print('Building sample size trace')
-    sample_size_trace = build_sample_size_trace(sorted_data)
-
-    # build heuristic trace
-    print('Building heuristic trace')
-    heuristic_trace = build_rssi_trace(sorted_data, log)
-
-    # build org trace
-    print('Building log measurement trace')
-    org_trace = build_rssi_trace(org_data, 'original')
-
-    # build normal distribution plot
-    print('Building normal distribution trace')
-    normal_plot = build_normal_plot(sorted_data)
-
-    # compute the score
-    print('Computing the score')
-    score = compute_score(sorted_data, org_data)
-
+    log_name = parse_logfile_name(log)
     layout = go.Layout(
-        xaxis=go.layout.XAxis(
-            tickmode='array',
-            tickvals=gen_x_axis(),
-            ticktext=x_axis_to_string(),
-            showline=False,
-            title='Angle buckets'
+        xaxis=dict(
+            title='Difference in RSSI minus distance fading between a link pair'
         ),
-        yaxis=go.layout.YAxis(
-            showline=True,
-            showgrid=False,
-            dtick=5,
-            title='RSSI'
+        yaxis=dict(
+            title='Count'
         ),
-        yaxis2=dict(
-            side='right',
-            title='Sample size',
-            overlaying='y',
-            showline=True,
-            tickmode='auto',
-            showgrid=False
-        ),
-        showlegend=True,
-        title=f'Heuristic comparison - Score: {round(score, 2)}'
+        title=f'{log_name} - Normalized - Sample size: {count}'
     )
-
-    fig = go.Figure(data=[org_trace, heuristic_trace, sample_size_trace], layout=layout)
-    rssi_div = plotly.offline.plot(fig, include_plotlyjs=True, output_type='div')
-
-    with open(f'plots/original-{log}.html', 'w') as f:
-        f.writelines(rssi_div)
-        f.writelines(normal_plot)
+    fig = go.Figure(data=data, layout=layout)
+    plotly.offline.plot(fig, filename=f'plots/histogram_normalized_{log_name}.html')
 
 
 if __name__ == '__main__':
-    log_path = '../../src/logs/parsed'
-    logs = os.listdir(log_path)
-    link_pairs = {}
-
+    # np.seterr(divide='ignore', invalid='ignore')
     print('Loading data from logs')
-    for log in logs:
-        if log.endswith('.csv'):
-            f_name, _ = os.path.splitext(log)
-        else:
-            f_name = log
+    # build plot for showing average rssi in a distance bucket
 
-        link_pairs[f_name] = parse(f'{log_path}/{f_name}')
+    for arg in sys.argv[1:2]:
+        # build plot presenting the average stochastic for angle buckets
+        # build_angle_bucket_stochastic_minus_distance_fading_increase_plot(arg)
 
-    for log in [i for i in logs if not i == 'original']:
-        if log.endswith('.csv'):
-            f_name, _ = os.path.splitext(log)
-        else:
-            f_name = log
+        # build normal distribution plot for angle buckets with distance fading
+        # build_normal_dist_for_angle_bucket(arg)
 
-        build_traces(link_pairs[f_name], format_trace_data(link_pairs['original']), f_name)
+        # build normal distribution plot for distance buckets with distance fading
+        # build_normal_dist_for_distance_bucket(arg)
 
-    print("done")
+        # build histogram for frequency of stochastic value only in angle buckets
+        build_histogram_for_stochastic_in_angle_buckets(arg)
+
+        # build bar plot for average rssi in a distance bucket
+        # build_average_rssi_for_distance_plot(arg)
--- tools/linkmodel_visualiser/models.py
+++ tools/linkmodel_visualiser/models.py
@@ -1,25 +1,43 @@
 import numpy as np
 import math
 
-np.seterr(all='raise')
+
+# np.seterr(all='raise')
 
 
 class Link:
-    def __init__(self, lat1: str, lon1: str, lat2: str, lon2: str, rssi: str):
+    def __init__(self, lat1: str, lon1: str, lat2: str, lon2: str, rssi: str, timestamp: str, node1_id: str,
+                 node2_id: str, remove_distance: bool):
+        self.timestamp = float(timestamp)
+        self.node1_id = int(node1_id)
+        self.node2_id = int(node2_id)
         self.node1 = [float(lat1), float(lon1)]
         self.node2 = [float(lat2), float(lon2)]
-        self.rssi = float(rssi) - (-self._l_d(self._distance_between()))
+        self.distance = self._distance_between() * 1000
+        self.rssi = (26 - Link.l_d(self.distance)) - float(rssi) if remove_distance else float(rssi)
 
     def __eq__(self, other):
-        return (self.node1[0] == other.node1[0] and self.node1[1] == other.node1[1] and self.node2[0] == other.node2[0]
-                and self.node2[1] == other.node2[1]) \
-               or (self.node2[0] == other.node1[0]
-                   and self.node2[1] == other.node1[1]
-                   and self.node1[0] == other.node2[0]
-                   and self.node1[1] == other.node2[1])
+        return ((self.node1[0] == other.node1[0] and self.node1[1] == other.node1[1]
+                 and self.node2[0] == other.node2[0] and self.node2[1] == other.node2[1])
+                or (self.node2[0] == other.node1[0] and self.node2[1] == other.node1[1]
+                    and self.node1[0] == other.node2[0] and self.node1[1] == other.node2[1]))
 
-    def _l_d(self, distance: float) -> float:
-        return 0.0 if distance == 0 else 55 * math.log10(1000 * distance) - 18
+    def __repr__(self) -> str:
+        return f'timestamp: {self.timestamp}, node1_id: {self.node1_id}, node1: {self.node1}, node2_id: {self.node2_id}, node2: {self.node2}, distance: {self.distance}, rssi: {self.rssi}'
+
+    def has_common_node(self, other) -> bool:
+        return (self.node1 == other.node1
+                or self.node1 == other.node2
+                or self.node2 == other.node1
+                or self.node2 == other.node2)
+
+    @staticmethod
+    def l_d(distance) -> float:
+        return 0.0 if distance == 0 else 25 * math.log10(distance) + 45
+        # return 0.0 if self.distance == 0 else 25 * math.log10(self.distance) + 45.8
+
+    def reverse_link(self, link) -> bool:
+        return link.node1 == self.node2 and link.node2 == self.node1
 
     def _distance_between(self) -> float:
         earth_radius = 6373.0
@@ -43,7 +61,7 @@ class LinkPair:
         self.link1 = link1
         self.link2 = link2
         self.angle = self.angle_between()
-        self.rssi = link1.rssi - link2.rssi if link1.rssi > link2.rssi else link2.rssi - link1.rssi
+        self.rssi_diff = abs(self.link1.rssi - self.link2.rssi)
 
     def __eq__(self, other):
         return (self.link1 == other.link1 and self.link2 == other.link2) or (
